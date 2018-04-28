f408d71a32e4b4e09f05af94535c404415b179f6
Kurt Dacudag <dcdgblessie18@gmail.com>
2018-04-28 13:40:29 +0800

    Initialize

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Globe\Globe;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+	public function __construct()
+	{
+		$this->passphrase = env('GLOBE_PASSPHRASE');
+		$this->app_id = env('GLOBE_APP_ID');
+		$this->app_secret = env('GLOBE_APP_SECRET');
+	}
+    public function index()
+    {
+    	$number = '';
+    	$message = '';
+    	$message = Globe::send($number, $message, $this->app_secret, $this->app_id, $this->app_secret);
+    }
+}
---
+++ config/globe.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'short_code' => env('GLOBE_SHORTCODE', '0344')
+];
